1a9f4896353d84bf3b20e4c04080f249880b49e8
Ivan Karazanov <dualking1991@gmail.com>
2024-01-11 18:49:11 +0300

    models

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
+from models.trade import Trade
+from models.user import User
 
 
 app = FastAPI(
@@ -14,9 +16,21 @@ fake_users = [
 ]
 
 
-@app.get("/users/{user_id}")
-def get_user(user_id: int) -> list:
-    return [user for user in fake_users if user.get("id") == user_id]
+@app.get("/users/all", response_model=list[User])
+def get_users() -> list[dict]:
+    return fake_users
+
+
+@app.get("/users/{user_id}", response_model=User)
+def get_user(user_id: int) -> User:
+    return next(filter(lambda user: user.get("id") == user_id, fake_users))
+
+
+@app.patch("/users/{user_id}", response_model=User)
+def change_user_name(user_id: int, new_name: str) -> User:
+    current_user = next(filter(lambda user: user.get("id") == user_id, fake_users))
+    current_user["name"] = new_name
+    return current_user
 
 
 fake_trades = [
@@ -30,18 +44,10 @@ def get_trades(limit: int = 1, offset: int = 1) -> list:
     return fake_trades[offset:][:limit]
 
 
-fake_users2 = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"}
-]
-
-
-@app.patch("/users/{user_id}")
-def change_user_name(user_id: int, new_name: str) -> dict:
-    current_user = next(filter(lambda user: user.get("id") == user_id, fake_users2))
-    current_user["name"] = new_name
-    return {"status": 200, "data": current_user}
+@app.post("/trades", response_model=list[Trade])
+def add_trades(trades: list[dict]) -> dict:
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
 
 
 if __name__ == "__main__":
